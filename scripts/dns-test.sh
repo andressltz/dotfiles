ca5c76874ba01ccde0def3d377f6fea618823545
Ândres Winck Schultz <andreswinck@cwi.com.br>
2025-10-22 15:18:19 -0300

    add scripts

---
+++ scripts/dns-test.sh
@@ -0,0 +1,14 @@
+echo "🏃‍♂️Testing G1 with Claro:"
+nslookup g1.globo.com 181.213.132.2
+
+echo "🏃‍♂️Testing G1 with Google:"
+nslookup g1.globo.com 8.8.8.8
+
+echo "🏃‍♂️Testing G1 with CloudFlare:"
+nslookup g1.globo.com 1.1.1.1
+
+echo "🏃‍♂️Testing G1 with Google 2:"
+nslookup g1.globo.com 8.8.4.4
+
+echo "🏃‍♂️Testing G1 with Quad9:"
+nslookup g1.globo.com 9.9.9.9
