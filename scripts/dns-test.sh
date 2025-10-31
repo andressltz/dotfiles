2ea9993413a57fac1699a5f474e60989493b2392
Ândres Winck Schultz <andreswinck@cwi.com.br>
2025-10-31 17:00:21 -0300

    update scripts

--- scripts/dns-test.sh
+++ scripts/dns-test.sh
@@ -1,14 +1,23 @@
 echo "🏃‍♂️Testing G1 with Claro:"
 nslookup g1.globo.com 181.213.132.2
 
+echo "🏃‍♂️Testing Tecnoblog with Claro:"
+nslookup tecnoblog.net 181.213.132.2
+
 echo "🏃‍♂️Testing G1 with Google:"
 nslookup g1.globo.com 8.8.8.8
+
+echo "🏃‍♂️Testing Tecnoblog with Google:"
+nslookup tecnoblog.net 8.8.8.8
 
 echo "🏃‍♂️Testing G1 with CloudFlare:"
 nslookup g1.globo.com 1.1.1.1
 
-echo "🏃‍♂️Testing G1 with Google 2:"
-nslookup g1.globo.com 8.8.4.4
+echo "🏃‍♂️Testing Tecnoblog with CloudFlare:"
+nslookup tecnoblog.net 1.1.1.1
 
 echo "🏃‍♂️Testing G1 with Quad9:"
 nslookup g1.globo.com 9.9.9.9
+
+echo "🏃‍♂️Testing Tecnoblog with Quad9:"
+nslookup tecnoblog.net 9.9.9.9
