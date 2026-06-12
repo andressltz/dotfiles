cf46ccf91e4d240c0b45616c3a60d5caff98d70c
Ândres Winck Schultz <andreswinck@cwi.com.br>
2026-06-12 10:26:47 -0300

    update remove script

--- scripts/remove.sh
+++ scripts/remove.sh
@@ -6,9 +6,6 @@
 #
 # This script removes Power Manager from the target drive. A restart is required.
 
-# TARGET_DRIVE="$3"
-TARGET_DRIVE="/"
-
 # /Library/LaunchAgents
 # /Library/LaunchDaemons
 # ~/Library/LaunchAgents
@@ -21,85 +18,91 @@ TARGET_DRIVE="/"
 # sudo systemextensionsctl list
 # sudo systemextensionsctl uninstall 53AQ936H96 org.pqrs.Karabiner-DriverKit-VirtualHIDDevice
 
+# find / \
+#   \( -path "*com.apple.TimeMachine.localsnapshots*" \
+#      -o -path "/System/Volumes/Preboot*" \
+#      -o -path "/System/Volumes/VM*" \) \
+#   -prune -o \
+#   -iname "*OneDrive*" -print 2>/dev/null
+
 # Ensure script has root authority
 if ! [ "$(id -u)" = 0 ]; then
   echo "You must be root to run this script" 2>&1
   exit 1
 fi
 
-##
-# Remove static files
 REMOVE_FILES=(
-  # ...PM5 only
-  'Library/LaunchDaemons/uk.co.dssw.powermanager.pmd.plist'  # Launchd job ticket
-  'Library/Logs/uk.co.dssw.powermanager' # daemon log files; PM5.10.1+
-  # ...PM3+PM4
-  'Applications/Power Manager.app' # Application
-  'etc/pam.d/uk.co.dssw.powermanager' # PAM policy
-  'Library/Application Support/Power Manager' # Shared tools and core engine
-  'Library/Automator/PMCancelEvents.action'
-  'Library/Automator/PMDelayEvents.action'
-  'Library/Automator/PMDeleteEvents.action'
-  'Library/Automator/PMEvent.definition'
-  'Library/Automator/PMEventsToStrings.caction'
-  'Library/Automator/PMExportEvents.action'
-  'Library/Automator/PMFilterEvents.action'
-  'Library/Automator/PMImportEvents.action'
-  'Library/Automator/PMListEvents.action'
-  'Library/Automator/PMNewEvent.action'
-  'Library/Automator/PMNextEvent.action'
-  'Library/Automator/PMResetEvents.action'
-  'Library/Automator/PMToggleScheduler.action'
-  'Library/Frameworks/PowerManager.framework' # Shared framework
-  'Library/LaunchAgents/uk.co.dssw.pmnotify.login.plist' # Launchd job ticket
-  'Library/LaunchAgents/uk.co.dssw.pmnotify.plist' # Launchd job ticket
-  'Library/LaunchAgents/uk.co.dssw.pmuser.plist' # Launchd job ticket
-  'Library/LaunchDaemons/uk.co.dssw.pmd.plist' # Launchd job ticket
-  'Library/LaunchDaemons/uk.co.dssw.powermanager.installer.plist' # Automated installer job ticket
-  'Library/PrivilegedHelperTools/uk.co.dssw.powermanager.installer' # Automated installer
-  'Library/Caches/uk.co.dssw.powermanager.certtool.cache.json' # certtool cache
-  'var/tmp/uk.co.dssw.powermanager' # Unix socket directory
-  # ...PM3 only
-  'Library/Documentation/Help/Power Manager Help' # Help Book symlink
-  'Library/PreferencePanes/Power Manager.prefPane' # Preference pane
-  'Library/StartupItems/PowerManager' # StartupItem (Mac OS X 10.3.9)
-  'Library/Widgets/Power Manager.wdgt' # Dashboard widget (Mac OS X 10.4+)
-  'System/Library/CoreServices/SecurityAgentPlugins/pmauth.bundle' # loginwindow plugin
-  'usr/local/powermanager' # Unix daemon
-  'Library/LaunchAgents/uk.co.dssw.pmnotify.plist'
-  'Library/LaunchAgents/uk.co.dssw.pmuser.plist'
-  'Library/LaunchDaemons/uk.co.dssw.powermanager.pmd.plist'
-  'System/Volumes/Data/Library/LaunchAgents/uk.co.dssw.pmnotify.plist'
-  'System/Volumes/Data/Library/LaunchAgents/uk.co.dssw.pmuser.plist'
-  'System/Volumes/Data/Library/LaunchDaemons/uk.co.dssw.powermanager.pmd.plist'
-  'System/Volumes/Data/private/var/db/receipts/uk.co.dssw.powermanager.launchdaemons.plist'
-  'System/Volumes/Data/private/var/db/receipts/uk.co.dssw.powermanager.launchdaemons.bom'
-  'System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/uk.co.dssw.powermanager.client.pmnotify'
-  'System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/uk.co.dssw.powermanager.client.pmnotify/uk.co.dssw.powermanager.client.pmnotify'
-  'System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/uk.co.dssw.powermanager.standard'
-  'System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/uk.co.dssw.powermanager.standard/uk.co.dssw.powermanager.standard'
-  'System/Volumes/Data/Users/andres/Library/Application Scripts/uk.co.dssw.update.feed'
-  'System/Volumes/Data/Users/andres/Library/Containers/uk.co.dssw.update.feed'
-  'System/Volumes/Data/Users/andres/Library/Containers/uk.co.dssw.update.feed/Data/Library/Application Scripts/uk.co.dssw.update.feed'
-  'Library/CoreMediaIO/Plug-Ins/DAL/SnapCamera.plugin'
-  'Library/LaunchDaemons/com.snap.SnapCameraRemover.plist'
-  'System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera'
-  'System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera/com.apple.metal'
-  'System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera/com.apple.metal/16777235_530'
-  'System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera/com.apple.metal/16777235_530/functions.data'
-  'System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera/com.apple.metal/16777235_530/functions.list'
-  'System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera/com.apple.metal/32024'
-  'System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera/com.apple.metal/32024/libraries.list'
-  'System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera/com.apple.metal/32024/libraries.data'
-  'System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera/com.apple.metalfe'
-  'System/Volumes/Data/Users/andres/Library/Caches/com.plausiblelabs.crashreporter.data/com.snap.SnapCamera'
-  'private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera'
-  'Library/SystemExtensions/0535073D-C057-4993-BE3F-28160EE596D8/org.pqrs.Karabiner-DriverKit-VirtualHIDDevice.dext'
-  'Library/SystemExtensions/0535073D-C057-4993-BE3F-28160EE596D8/org.pqrs.Karabiner-DriverKit-VirtualHIDDevice.dext/org.pqrs.Karabiner-DriverKit-VirtualHIDDevice'
+  '/Library/LaunchDaemons/uk.co.dssw.powermanager.pmd.plist'  # Launchd job ticket
+  '/Library/Logs/uk.co.dssw.powermanager' # daemon log files; PM5.10.1+
+  '/Applications/Power Manager.app' # Application
+  '/etc/pam.d/uk.co.dssw.powermanager' # PAM policy
+  '/Library/Application Support/Power Manager' # Shared tools and core engine
+  '/Library/Automator/PMToggleScheduler.action'
+  '/Library/Frameworks/PowerManager.framework' # Shared framework
+  '/Library/LaunchAgents/uk.co.dssw.pmnotify.login.plist' # Launchd job ticket
+  '/Library/LaunchAgents/uk.co.dssw.pmnotify.plist' # Launchd job ticket
+  '/Library/LaunchAgents/uk.co.dssw.pmuser.plist' # Launchd job ticket
+  '/Library/LaunchDaemons/uk.co.dssw.powermanager.installer.plist' # Automated installer job ticket
+  '/Library/PrivilegedHelperTools/uk.co.dssw.powermanager.installer' # Automated installer
+  '/Library/Caches/uk.co.dssw.powermanager.certtool.cache.json' # certtool cache
+  '/Library/Documentation/Help/Power Manager Help' # Help Book symlink
+  '/Library/PreferencePanes/Power Manager.prefPane' # Preference pane
+  '/Library/StartupItems/PowerManager' # StartupItem (Mac OS X 10.3.9)
+  '/Library/Widgets/Power Manager.wdgt' # Dashboard widget (Mac OS X 10.4+)
+  '/System/Library/CoreServices/SecurityAgentPlugins/pmauth.bundle' # loginwindow plugin
+  '/Library/LaunchAgents/uk.co.dssw.pmuser.plist'
+  '/Library/LaunchDaemons/uk.co.dssw.powermanager.pmd.plist'
+  '/System/Volumes/Data/Users/andres/Library/Containers/uk.co.dssw.update.feed'
+  '/Library/CoreMediaIO/Plug-Ins/DAL/SnapCamera.plugin'
+  '/Library/LaunchDaemons/com.snap.SnapCameraRemover.plist'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera/com.apple.metal'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera/com.apple.metal/16777235_530'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera/com.apple.metal/16777235_530/functions.data'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera/com.apple.metal/16777235_530/functions.list'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera/com.apple.metal/32024'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera/com.apple.metal/32024/libraries.list'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera/com.apple.metal/32024/libraries.data'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera/com.apple.metalfe'
+  '/System/Volumes/Data/Users/andres/Library/Caches/com.plausiblelabs.crashreporter.data/com.snap.SnapCamera'
+  '/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.snap.SnapCamera'
+  '/Library/SystemExtensions/0535073D-C057-4993-BE3F-28160EE596D8/org.pqrs.Karabiner-DriverKit-VirtualHIDDevice.dext'
+  '/Library/SystemExtensions/0535073D-C057-4993-BE3F-28160EE596D8/org.pqrs.Karabiner-DriverKit-VirtualHIDDevice.dext/org.pqrs.Karabiner-DriverKit-VirtualHIDDevice'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/0/sysdiagnose.616-3Tirjz/usernoted/apps/com.ideashower.ReadItLaterPro.txt'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.ideashower.ReadItLaterPro'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.ideashower.ReadItLaterPro/com.ideashower.ReadItLaterPro'
+  '/System/Volumes/Data/Users/andres/Library/Group Containers/group.com.apple.UserNotifications/Library/UserNotifications/Remote/default/SectionSettings/com.ideashower.ReadItLaterPro.sectionsettings'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.microsoft.rdc.macos'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.microsoft.rdc.macos/com.microsoft.rdc.macos'
+  '/System/Volumes/Data/Users/andres/Library/Application Scripts/UBF8T346G9.com.microsoft.rdc'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.microsoft.rdc.macos'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.microsoft.rdc.macos/com.microsoft.rdc.macos'
+  '/System/Volumes/Data/Users/andres/Library/Application Scripts/UBF8T346G9.com.microsoft.rdc'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.surteesstudios.Bartender'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.surteesstudios.Bartender5StartAtLoginHelper'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.surteesstudios.HideMenuBarHelper'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.surteesstudios.HideMenuBarHelper/com.surteesstudios.HideMenuBarHelper'
+  '/System/Volumes/Data/Users/andres/Library/Daemon Containers/64B2A130-01F5-4F36-9295-C235C808254D/Data/ActionTranscript/com.surteesstudios.Bartender'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/developer.apple.wwdc-Release'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/developer.apple.wwdc-Release/developer.apple.wwdc-Release'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/0/sysdiagnose.616-3Tirjz/usernoted/apps/com.microsoft.OneDrive-mac.txt'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.microsoft.OneDrive-mac'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.microsoft.OneDrive-mac/com.microsoft.OneDrive-mac'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.microsoft.OneDrive-mac.FileProvider'
+  '/System/Volumes/Data/private/var/folders/wj/b3y8wmt95x57gkd_sbjgmhhc0000gn/C/com.microsoft.OneDrive-mac.FinderSync'
+  '/System/Volumes/Data/Users/andres/Library/Application Support/FileProvider/com.microsoft.OneDrive-mac.FileProvider'
+  '/System/Volumes/Data/Users/andres/Library/Application Support/FileProvider/com.microsoft.OneDrive-mac.FileProvider/OneDrive - UNIÃO DOS ESCOTEIROS DO BRASIL'
+  '/System/Volumes/Data/Users/andres/Library/Preferences/com.apple.FileProvider/com.microsoft.OneDrive-mac.FileProvider'
+  '/System/Volumes/Data/Users/andres/Library/Preferences/com.apple.FileProvider/com.microsoft.OneDrive-mac.FileProvider/OneDrive - UNIÃO DOS ESCOTEIROS DO BRASIL.plist'
+  '/System/Volumes/Data/Users/andres/Library/Preferences/com.apple.FileProvider/com.microsoft.OneDrive-mac.FileProvider/OneDrive.plist'
+  '/System/Volumes/Data/Users/andres/Library/Application Scripts/UBF8T346G9.OneDriveSyncClientSuite'
+  '/System/Volumes/Data/Users/andres/Library/Application Scripts/UBF8T346G9.OfficeOneDriveSyncIntegration'
+  '/System/Volumes/Data/Users/andres/Library/Group Containers/UBF8T346G9.OfficeOneDriveSyncIntegration'
+  '/System/Volumes/Data/Users/andres/Library/Group Containers/UBF8T346G9.OfficeOneDriveSyncIntegration/Library/Application Scripts/UBF8T346G9.OfficeOneDriveSyncIntegration'
   );
 for (( i = 0 ; i < "${#REMOVE_FILES[@]}" ; i++ ))
 do
-  FILEPATH="${TARGET_DRIVE}${REMOVE_FILES[$i]}"
+  FILEPATH="${REMOVE_FILES[$i]}"
   if [ -e "$FILEPATH" ]; then
     echo "Removing: ${FILEPATH}"
     rm -rf "${FILEPATH}"
@@ -108,9 +111,27 @@ done
 
 ##
 # Remove the run time created files
-find "${TARGET_DRIVE}private/var/db/receipts" -name 'Power Manager *.pkg' -type d -print0 | xargs -0 rm -rf
-find "${TARGET_DRIVE}Library/Receipts" -name 'Power Manager *.pkg' -type d -print0 | xargs -0 rm -rf
-find "${TARGET_DRIVE}Library/Preferences" -name 'uk.co.dssw.powermanager.*' -type f -delete
-find "${TARGET_DRIVE}var/root/Library/Preferences" -name 'uk.co.dssw.powermanager.*' -type f -delete
+find "/private/var/db/receipts" -name 'Power Manager *.pkg' -type d -print0 | xargs -0 rm -rf
+find "/Library/Receipts" -name 'Power Manager *.pkg' -type d -print0 | xargs -0 rm -rf
+find "/Library/Preferences" -name 'uk.co.dssw.powermanager.*' -type f -delete
+find "/var/root/Library/Preferences" -name 'uk.co.dssw.powermanager.*' -type f -delete
+
+find "/Library/Preferences" -name 'com.ideashower.*' -type f -delete
+find "/var/root/Library/Preferences" -name 'com.ideashower.*' -type f -delete
+
+find "/Library/Preferences" -name 'com.microsoft.OneDrive*' -type f -delete
+find "/var/root/Library/Preferences" -name 'com.microsoft.OneDrive*' -type f -delete
+
+find "/Library/Preferences" -name 'com.microsoft.rdc.*' -type f -delete
+find "/var/root/Library/Preferences" -name 'com.microsoft.rdc.*' -type f -delete
+
+find "/Library/Preferences" -name 'com.surteesstudios.*' -type f -delete
+find "/var/root/Library/Preferences" -name 'com.surteesstudios.*' -type f -delete
+
+find "/Library/Preferences" -name 'com.snap.*' -type f -delete
+find "/var/root/Library/Preferences" -name 'com.snap.*' -type f -delete
+
+find "/Library/Preferences" -name 'org.pqrs.*' -type f -delete
+find "/var/root/Library/Preferences" -name 'org.pqrs.*' -type f -delete
 
 exit 0
